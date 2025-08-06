6bcbdad1a964746b7d9306e22fdbfee59727e4d8
Chip Bell <cbell@pbs.org>
2025-08-06 13:33:23 -0400

    Fixing some deprecations as part of the AWS provider update

--- data.tf
+++ data.tf
@@ -48,8 +48,8 @@ data "aws_iam_policy_document" "default_role_policy_json" {
       "ssm:GetParameter"
     ]
     resources = [
-      "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter${local.ssm_path}",
-      "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter${local.ssm_path}*"
+      "arn:aws:ssm:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:parameter${local.ssm_path}",
+      "arn:aws:ssm:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:parameter${local.ssm_path}*"
 
     ]
   }
